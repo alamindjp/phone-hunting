a9bf2ea243c94bb3d690b149bedc52df1b1056fe
Md. Alamin Hossain <alaminhossaindjp@gmail.com>
2022-03-01 18:06:49 +0600

    update

--- js/app.js
+++ js/app.js
@@ -17,51 +17,9 @@ const searchPhone = () => {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
         fetch(url)
             .then(res => res.json())
-            .then(data => searchResultDisplay(data.data))
+            .then(data => searchResultDisplay(data.data.slice(0, 20)))
         // .catch(error => displayError(error))
 
     }
 
-}
-const searchResultDisplay = phones => {
-    // console.log(phones)
-    const searchResult = document.getElementById('search-result')
-    searchResult.textContent = '';
-    if (!phones.length) {
-        const errorField = document.getElementById('search-result')
-        const errorMassage = document.createElement('div')
-        errorMassage.innerHTML = `<p class="text-center text-danger fs-2 fw-bold"> Result not found</p >`
-        errorField.appendChild(errorMassage);
-        // console.log('test')
-
-    }
-    else {
-        phones.forEach(phone => {
-            console.log(phone.slug)
-            const div = document.createElement('div')
-            div.classList.add('col-lg-4')
-            div.classList.add('col-sm-6')
-            div.innerHTML = `
-            <div id="phone-card" class="border-0 shadow-lg mx-3 card h-100">
-                <img src="${phone.image}" class="card-img-top w-75 mx-auto mt-5" alt="...">
-                <div class="card-body ms-2 my-3">
-                    <h5 class="card-title ">${phone.phone_name}</h5>
-                    <h4 class="card-text">${phone.brand}</h4>
-                    <button onclick="phoneDetails(${phone.slug.id})" class="btn bg-info mt-3 w-50" type="button" id="details-button">Details</button>
-                </div>
-            </div>  
-        `
-            searchResult.appendChild(div)
-        });
-    }
-}
-
-const phoneDetails = (id) => {
-    // debugger;
-    const url = `"https://openapi.programming-hero.com/api/phone/${id}"`;
-    console.log('test', id)
-    // fetch(url)
-    //     .then(res => res.json())
-    //     .then(data => console.log(data));
-    // // // .catch (error=> displayError(error))
 }
