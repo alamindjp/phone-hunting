00ef4e94ec2bb9518c0a214bedd9595495d2a484
Md. Alamin Hossain <alaminhossaindjp@gmail.com>
2022-03-01 18:08:23 +0600

    update

--- js/app.js
+++ js/app.js
@@ -23,3 +23,35 @@ const searchPhone = () => {
     }
 
 }
+const searchResultDisplay = phones => {
+    // console.log(phones)
+    const searchResult = document.getElementById('search-result')
+    searchResult.textContent = '';
+    if (!phones.length) {
+        const errorField = document.getElementById('search-result')
+        const errorMassage = document.createElement('div')
+        errorMassage.innerHTML = `<p class="text-center text-danger fs-2 fw-bold"> Result not found</p >`
+        errorField.appendChild(errorMassage);
+        // console.log('test')
+
+    }
+    else {
+        phones.forEach(phone => {
+            // console.log(phone)
+            const div = document.createElement('div')
+            div.classList.add('col-lg-4')
+            div.classList.add('col-sm-6')
+            div.innerHTML = `
+            <div id="phone-card" class="border-0 shadow-lg mx-3 card h-100">
+                <img src="${phone.image}" class="card-img-top w-75 mx-auto mt-5" alt="...">
+                <div class="card-body ms-2 my-3">
+                    <h5 class="card-title ">${phone.phone_name}</h5>
+                    <h4 class="card-text">${phone.brand}</h4>
+                    <button onclick="phoneDetails('${phone.slug}')" class="btn bg-info mt-3 w-50" type="button" id="details-button">Details</button>
+                </div>
+            </div>  
+        `
+            searchResult.appendChild(div)
+        });
+    }
+}
